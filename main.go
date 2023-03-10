5e8663c0f75de059179687f74cc51d537df6d785
ervinanovlianti <ervinanov03@gmail.com>
2023-03-10 12:01:16 +0800

    p

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+ 
+import "fmt"
+ 
+func main() {
+    a := "Codingrakitan.blogspot.com" 
+    b := 7 
+    c := true 
+    fmt.Println("a = ", a)
+    fmt.Println("b = ", b)
+    fmt.Println("c = ", c)
+}

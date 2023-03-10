2d9a57bd7fc4715ce75c506ac15e2fcd63845770
ervinanovlianti <ervinanov03@gmail.com>
2023-03-10 12:03:07 +0800

    belajar-golang

--- main.go
+++ main.go
@@ -3,10 +3,5 @@ package main
 import "fmt"
  
 func main() {
-    a := "Codingrakitan.blogspot.com" 
-    b := 7 
-    c := true 
-    fmt.Println("a = ", a)
-    fmt.Println("b = ", b)
-    fmt.Println("c = ", c)
+	fmt.Sprintf("Belajar Golang")
 }
